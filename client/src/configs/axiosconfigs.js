d0b03d639fcbf1981467ecbf1a262371c23d280f
ArcaneWizard <42625247+ArcaneWizard@users.noreply.github.com>
2021-10-24 07:06:16 -0700

    hello

--- client/src/configs/axiosconfigs.js
+++ client/src/configs/axiosconfigs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://mu-sync.herokuapp.com/" || "http://localhost:5000/",
+  baseURL: process.env.RELATIVE_URL || "http://localhost:5000/",
 });
 
 export default instance;
